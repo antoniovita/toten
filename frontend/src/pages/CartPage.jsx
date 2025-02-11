766806898aa01691d9408aacbe2ffc6ca3392c59
antoniovita <antoniovitafonseca@gmail.com>
2025-02-11 12:01:40 -0300

    Começo das mudancças no frontend, falta fazer o sendOrder

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -63,7 +63,7 @@ export default function CartPage() {
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold ml-20">Carrinho</h1>
         <div className="flex justify-around gap-10 mr-20">
-          <Link to={"/"}>
+          <Link to={"/menu"}>
           <h1 className="text-2xl font-bold mb-4 text-center text-blue-500 my-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-utensils">
               <path d="M3 2v7c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2V2"/>
